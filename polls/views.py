cdc0ebda6d57d653d02ae659f8bb53ae1e6b757d
binmamun <bizzmamun@gmail.com>
2020-06-25 23:20:17 +0600

    created models

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
- print ('Hello world')
+
+def index(request):
+    return HttpResponse('Hello world!')
